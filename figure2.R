617741d858b02e3c467517c448a933a0ae1e0a01
tomoyahiroe <tomoya4creative@gmail.com>
2023-07-22 15:28:46 +0900

    refactor: コードを整形

--- figure2.R
+++ figure2.R
@@ -1,4 +1,4 @@
-#### Import libraries ####
+# 必要なライブラリの読み込み
 library(ggplot2)
 library(tidyverse)
 library(openxlsx)
@@ -6,65 +6,66 @@ library(vars)
 library(grid)
 library(gridExtra)
 
-#### Load modules in other files ####
+# 別ファイルに定義した関数の読み込み
 source("modules/trivar.R")
 source("modules/make_matrix.R")
 
-#### data import ####
+# データの読み込み
 data <- make_matrix("data/data.txt")
 
-#### VARモデルによる指定と結果の取得 ####
+# VARモデルによる指定と結果の取得
 result_list <- trivar(data)
 Ehat <- result_list$Ehat
 
-#### なぜかEhatの各行の最初の1,2要素の平均とEhatの各行を結合している ####
+# Ehatの各行の最初の1,2要素の平均と結合
 q1 <- c(mean(Ehat[1, 1:2]), Ehat[1, ]) 
-    # なぜEhat[1,1:2]で最初の二つの平均を取っている？
-    # そして、Ehat[1,]と結合している？？
 q2 <- c(mean(Ehat[2, 1:2]), Ehat[2, ])
 q3 <- c(mean(Ehat[3, 1:2]), Ehat[3, ])
 
-#### それぞれのデータの必要な長さを定義 ####
+# 各データの必要な長さを定義
 time <- 1975:2007
 q1a <- numeric(length(time))
 q2a <- numeric(length(time))
 q3a <- numeric(length(time))
 
-#### なんか平均を上記で定義した配列に格納している ####
+# 平均を上記で定義した配列に格納
 for (i in 1:length(time)) {
   start_index <- 12 * (i - 1) + 1
   end_index <- 12 * i
-  # なんで以下のようなことが必要なのか??
   q1a[i] <- mean(q1[start_index:end_index])
   q2a[i] <- mean(q2[start_index:end_index])
   q3a[i] <- mean(q3[start_index:end_index])
 }
 
-#### データフレームの作成 ####
+# データフレームの作成
 dfq1a <- data.frame(time = time, q1a = q1a)
 dfq2a <- data.frame(time = time, q1a = q2a)
 dfq3a <- data.frame(time = time, q1a = q3a)
 
-#### ggplotでプロット ####
-figure2_ploter <- function(df, y_name, title) {
+# ggplotでプロットする関数
+figure2_plotter <- function(df, y_name, title) {
   result <- ggplot(df, aes(x = time, y = y_name)) +
-    geom_line(col = 4) +
+    geom_path(col = 4) +
     geom_hline(yintercept = 0, color = 3) +
     ggtitle(title) +
+    theme(plot.title = element_text(hjust = 0.5)) +
     xlim(1975, 2007) +
     ylim(-1, 1)
   return(result)
 }
-pl1 <- figure2_ploter(dfq1a, q1a, "Oil Supply Shock")
-pl2 <- figure2_ploter(dfq1a, q2a, "Aggregate Demand Shock")
-pl3 <- figure2_ploter(dfq1a, q3a, "Oil-Apecific Demand Shock")
 
+# 定義した関数を用いてプロット
+pl1 <- figure2_plotter(dfq1a, q1a, "Oil Supply Shock")
+pl2 <- figure2_plotter(dfq2a, q2a, "Aggregate Demand Shock")
+pl3 <- figure2_plotter(dfq3a, q3a, "Oil-Specific Demand Shock")
+
+# プロットをグリッド状に配置
 gridExtra::grid.arrange(
   pl1, 
   pl2, 
   pl3, 
   nrow = 3, 
-  top=textGrob("Figure 2: The Historical Evolution of the Structural Shocks\n 1975-2007")
+  top = textGrob(expression(bold("Figure 2: The Historical Evolution of the Structural Shocks 1975-2007")),
+                 gp = gpar(col = "black", fontsize = 10)
+  )
 )
-
-
--- figure4.R
+++ figure4.R
@@ -1,35 +1,39 @@
-# Load necessary libraries
+# 必要なライブラリの読み込み
 library(vars)
 library(ggplot2)
 library(grid)
 library(gridExtra)
 
-# Source the custom functions
-source("modules/trivar.R")
-# Run trivar function
-source("modules/make_matrix.R")
-# Load the data and prepare the matrix
-# Replace "data_matrix" with your data matrix containing V1, V2, and V3
+# 別ファイルに定義した関数の読み込み
+source("modules/trivar.R")   # inputは行列データ、outputはvar_model, Uhat, Ehat, SIGMA, time, h, p
+source("modules/make_matrix.R")   # inputはファイルパス、outputは行列化されたデータ
+
+# 読み込んだ関数の実行
 data_matrix <- make_matrix("data/data.txt")
-trivar_res <- trivar(data_matrix)  # Please replace your_matrix_here with the actual matrix.
+trivar_res <- trivar(data_matrix)
 
-# Compute structural multipliers using irf function from vars package
-IRF <- irf(trivar_res$var_model, impulse = colnames(trivar_res$Uhat), response = colnames(trivar_res$Uhat), boot = FALSE, n.ahead = length(trivar_res$time) - trivar_res$p - 1)
+# varsパッケージのirf関数で、インパルス応答関数を得る
+IRF <- irf(
+  trivar_res$var_model,
+  impulse = colnames(trivar_res$Uhat),
+  response = colnames(trivar_res$Uhat),
+  boot = FALSE,
+  n.ahead = length(trivar_res$time) - trivar_res$p - 1
+)
 
-# Compute structural shocks Ehat from reduced form shocks Uhat
+# 構造的ショックEhatを計算
 Ehat <- solve(t(chol(trivar_res$SIGMA))) %*% t(trivar_res$Uhat)
 
-
-# Cross-multiply the weights for the effect of a given shock on the real oil price (given by the relevant row of IRF) with the structural shock in question
+# 応答変数の予測値をそれぞれ時系列順に格納
 yhat1 <- yhat2 <- yhat3 <- rep(0, length(trivar_res$time) - trivar_res$p)
-
 for(i in 1:(length(trivar_res$time) - trivar_res$p)){
-  yhat1[i] <- sum(IRF$irf$V1[1:i,1] * Ehat[1, i:1])  # Replace your_var1_name with the actual variable name
-  yhat2[i] <- sum(IRF$irf$V2[1:i,2] * Ehat[2, i:1])  # Replace your_var2_name with the actual variable name
-  yhat3[i] <- sum(IRF$irf$V3[1:i,3] * Ehat[3, i:1])  # Replace your_var3_name with the actual variable name
+  yhat1[i] <- sum(IRF$irf$V1[1:i,1] * Ehat[1, i:1])  
+  yhat2[i] <- sum(IRF$irf$V2[1:i,2] * Ehat[2, i:1])  
+  yhat3[i] <- sum(IRF$irf$V3[1:i,3] * Ehat[3, i:1])  
 }
 
-# Create dataframe for plotting
+# データをプロット
+# プロットするためにデータフレームを作成
 plot_data1 <- data.frame(
   time = trivar_res$time[(trivar_res$p + 1):length(trivar_res$time)],
   yhat = yhat1
@@ -43,26 +47,32 @@ plot_data3 <- data.frame(
   yhat = yhat3
 )
 
-# Create plots
+# プロットのロジックを関数化
 figure4_plotter <- function(plot_data, yhat, subtitle) {
   result <- ggplot(plot_data, aes(x = time)) +
-    geom_line(aes(y = yhat), color = 'blue') +
-    ylim(-100, 100)+
+    geom_path(aes(y = yhat), color = 'blue', linewidth = 1) +
+    ylim(-100, 100) +
     labs(title = subtitle,
          y = "",                   
-         x = "Time") +
-    theme_minimal()
+         x = "") +
+    theme(plot.title = element_text(hjust = 0.5))
   return(result)
 }
 
+# 定義した関数を実行
 pl1 <- figure4_plotter(plot_data1, yhat1, "Cumulative Effect of Oil Supply Shock on Real Price of Crude Oil")
 pl2 <- figure4_plotter(plot_data2, yhat2, "Cumulative Effect of Aggregate Demand Shock on Real Price of Crude Oil")
 pl3 <- figure4_plotter(plot_data3, yhat3, "Cumulative Effect of Oil-Market Specific Demand Shock on Real Price of Crude Oil")
 
+# 縦一列にプロット
 gridExtra::grid.arrange(
   pl1, 
   pl2, 
   pl3, 
   nrow = 3, 
-  top=textGrob("Historical Decomposition of Real Price of Oil 1976.1-2007.12")
+  top = textGrob(
+    expression(
+      bold("Historical Decomposition of Real Price of Oil 1976.1-2007.12")),
+    gp = gpar(col = "black", fontsize = 20)
+  )
 )
